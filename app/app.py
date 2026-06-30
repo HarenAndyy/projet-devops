addc491c74cda1f51a99e031b789c76abed450fb
HarenAndyy <andyandriaa1@gmail.com>
2026-06-30 09:51:21 +0300

    feat: application Flask fonctionnelle v1.0.0

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import datetime, platform
+
+app = Flask(__name__)
+VERSION = "1.0.0"
+
+@app.route("/")
+def index():
+    msg = f"Déployé le {datetime.datetime.now().strftime('%d/%m/%Y à %H:%M:%S')}"
+    return f"""
+    <h1>Application DevOps</h1>
+    <p><strong>Version :</strong> {VERSION}</p>
+    <p><strong>Message :</strong> {msg}</p>
+    <p><strong>Serveur :</strong> {platform.node()}</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
